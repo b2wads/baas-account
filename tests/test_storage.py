f59cf1779449989edf3a7f6e9fd028aa77348662
Dalton Barreto <daltonmatos@gmail.com>
2020-03-16 14:08:34 -0300

    Implementando AccountStorage.clear()

---
+++ tests/test_storage.py
@@ -0,0 +1,18 @@
+from asynctest import TestCase
+
+from baas.account.models import Account
+from baas.services.account import AccountStorage
+
+
+class AccountStorageTest(TestCase):
+    async def setUp(self):
+        self.storage = AccountStorage()
+
+    async def test_clear_storage(self):
+        acc = Account(nome="Dalton Barreto", cpf="42")
+        self.storage.save(acc.cpf, acc)
+
+        self.assertEqual(acc.dict(), self.storage.get_by_id(acc.cpf))
+        self.storage.clear()
+
+        self.assertEqual(None, self.storage.get_by_id(acc.cpf))
