e5d50ae988ff8c88470c4f181f703e13a805566d
Dalton Barreto <daltonmatos@gmail.com>
2020-03-20 15:27:13 -0300

    Implementando decorator @app.http()
    Permite retornar diretamente modeos Pydantic ou lista de modelos.

--- baas/app.py
+++ baas/app.py
@@ -1,3 +1,33 @@
-from asyncworker import App
+from functools import wraps
+from typing import get_type_hints, List, Type
 
-app = App()
+from aiohttp import web
+from asyncworker import App, RouteTypes
+from asyncworker.http.wrapper import RequestWrapper
+from asyncworker.routes import call_http_handler
+
+
+class MyApp(App):
+    def http(self, routes: List[str], methods: List[str] = ["GET"]):
+        def _wrap(handler):
+            return self.route(routes, type=RouteTypes.HTTP, methods=methods)(
+                self._unwrap_pydantic_model(handler)
+            )
+
+        return _wrap
+
+    def _unwrap_pydantic_model(self, handler):
+        async def _handler_wrapper(wrapper: RequestWrapper):
+            response = await call_http_handler(wrapper.http_request, handler)
+            try:
+                if isinstance(response, list):
+                    return web.json_response([o.dict() for o in response])
+
+                return web.json_response(response.dict())
+            except AttributeError:
+                return response
+
+        return _handler_wrapper
+
+
+app = MyApp()
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,9 +1,15 @@
+from http import HTTPStatus
+from typing import List
+
 from aiohttp import web
 from asynctest import TestCase
-from asyncworker import App, RouteTypes
+from asyncworker import RouteTypes
+from asyncworker.http.decorators import parse_path
+from asyncworker.http.wrapper import RequestWrapper
 from asyncworker.testing import HttpClientContext
 from pydantic import BaseModel
 
+from baas.app import MyApp
 from baas.http import parse_body, parse_id
 
 
@@ -14,7 +20,7 @@ class Model(BaseModel):
 
 class HTTPDecoratorsTest(TestCase):
     async def setUp(self):
-        self.app = App()
+        self.app = MyApp()
 
         @self.app.route(["/body"], type=RouteTypes.HTTP, methods=["POST"])
         @parse_body(Model)
@@ -25,7 +31,7 @@ class HTTPDecoratorsTest(TestCase):
             ["/get_by_id/{id}"], type=RouteTypes.HTTP, methods=["POST"]
         )
         @parse_id(int)
-        async def parse_body_handler(id_: int):
+        async def parse_path_handler(id_: int):
             m = Model(name="Dalton", numero=id_)
             return web.json_response(m.dict())
 
@@ -42,3 +48,68 @@ class HTTPDecoratorsTest(TestCase):
             resp = await client.post("/get_by_id/42")
             data = await resp.json()
             self.assertEqual({"name": "Dalton", "numero": 42}, data)
+
+
+class HTTPRouteDecoratorTest(TestCase):
+    async def setUp(self):
+        self.app = MyApp()
+
+    async def test_registers_http_route(self):
+        @self.app.http(["/hello"])
+        async def _hello_handler():
+            return web.json_response({"Hello": True})
+
+        async with HttpClientContext(self.app) as client:
+            resp = await client.get("/hello")
+
+            self.assertEqual(HTTPStatus.OK, resp.status)
+            data = await resp.json()
+
+            self.assertEqual({"Hello": True}, data)
+
+    async def test_can_use_other_http_methods(self):
+        expected_response_data = {"POST": True}
+
+        @self.app.http(["/post"], methods=["POST"])
+        async def _get_post(wrapper: RequestWrapper):
+            body = await wrapper.http_request.json()
+            return web.json_response(body)
+
+        async with HttpClientContext(self.app) as client:
+            resp = await client.post("/post", json=expected_response_data)
+            self.assertEqual(HTTPStatus.OK, resp.status)
+
+            data = await resp.json()
+            self.assertEqual(expected_response_data, data)
+
+    async def test_can_return_pydantic_model(self):
+        class User(BaseModel):
+            id: str
+
+        @self.app.http(["/users/{user_id}"])
+        @parse_path
+        async def _get_user_by_id(user_id: str) -> User:
+            return User(id=user_id)
+
+        async with HttpClientContext(self.app) as client:
+            resp = await client.get("/users/42")
+            self.assertEqual(HTTPStatus.OK, resp.status)
+
+            data = await resp.json()
+            self.assertEqual(User(id="42").dict(), data)
+
+    async def test_can_return_list_of_pydnatic_models(self):
+        class User(BaseModel):
+            id: str
+
+        @self.app.http(["/users/{user_id}/{other_id}"])
+        @parse_path
+        async def _get_user_by_id(user_id: str, other_id: str) -> List[User]:
+            return [User(id=user_id), User(id=other_id)]
+
+        async with HttpClientContext(self.app) as client:
+            resp = await client.get("/users/42/44")
+            self.assertEqual(HTTPStatus.OK, resp.status)
+
+            data = await resp.json()
+            self.assertEqual([User(id="42").dict(), User(id="44").dict()], data)
