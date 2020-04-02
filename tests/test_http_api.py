898a544ed9f9bfc129c99807752d13325edeb5b2
Dalton Barreto <daltonmatos@gmail.com>
2020-04-02 14:59:19 -0300

    Implementação endpoints HTTP
    GET /accounts
    POST /accounts

--- tests/test_http_api.py
+++ tests/test_http_api.py
@@ -4,9 +4,14 @@ from asynctest import TestCase
 from asyncworker.testing import HttpClientContext
 
 from baas.api import app
+from baas.models import Account
+from baas.services.account import AccountService
 
 
 class AccountAPITest(TestCase):
+    async def setUp(self):
+        AccountService.storage.clear()
+
     async def test_health(self):
         async with HttpClientContext(app) as client:
             resp = await client.get("/health")
@@ -14,3 +19,28 @@ class AccountAPITest(TestCase):
             self.assertEqual(HTTPStatus.OK, resp.status)
             data = await resp.json()
             self.assertEqual({"OK": True}, data)
+
+    async def test_salva_conta(self):
+        async with HttpClientContext(app) as client:
+            self.fail()
+
+    async def test_lista_contas_banco_vazio(self):
+        async with HttpClientContext(app) as client:
+            resp = await client.get("/accounts")
+
+            self.assertEqual(HTTPStatus.OK, resp.status)
+            data = await resp.json()
+            self.assertEqual([], data)
+
+    async def test_lista_contas(self):
+        acc = Account(nome="Dalton", cpf="42")
+        async with HttpClientContext(app) as client:
+            await client.post("/accounts", json=acc.dict())
+            resp = await client.get("/accounts")
+
+            self.assertEqual(HTTPStatus.OK, resp.status)
+            data = await resp.json()
+
+            self.assertEqual(1, len(data))
+            acc_1 = Account(**data[0])
+            self.assertEqual(acc_1, acc)
